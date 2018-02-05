4480f186077721ef2e6f7db6b3b22ef722af9b68
PaulieWu <wqy630@qq.com>
2018-02-05 09:57:57 +0800

    modify: using trie tree

--- words_discover.py
+++ words_discover.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*
 import numpy as np
+import pygtrie as trie
 import sys
+import datetime
 
 def get_count_dict(string):
-    c_list = list(string)
-    c_set = set(c_list)
-    c_dict = {i: c_list.count(i) for i in c_set}
-    return c_dict
+    start = datetime.datetime.now()
+    print "start create count_dict ...".upper()
+    c_dict = trie.StringTrie()
+    for c in string:
+        if c in c_dict:
+            c_dict[c] += 1
+        else:
+            c_dict[c] = 1
+    end = datetime.datetime.now()
+    print ("create count_dict done, costs time: %s" % (str(end - start))).upper()
+    return c_dict   
 
 def get_information_entropy(c_list):
     total_freq = float(len(c_list))
@@ -23,17 +32,17 @@ def get_candidate_dict(string, min_l=2, max_l=5, tf=1, ie=0, pmi=1):
     return: list
     """
     string_l = len(string)
+    print "text length: %d" % (string_l)
     postfix_list = range(len(string))
-    print len(postfix_list)
-    postfix_list = sorted(postfix_list, cmp=lambda x, y: cmp(string[x:], string[y:]))
-    print postfix_list[0]
-    candidate_dict = {}
-    total_freq = 0          
+    candidate_dict = trie.StringTrie()
+    total_freq = 0
+    start = datetime.datetime.now()
+    print "start create candidate_dict ..."          
     for l in range(min_l, max_l + 1):
-        last_word = None
-        tmp_post_list = []
-        tmp_pre_list = []
-        tmp_tf = 0
+        # last_word = string[postfix_list[0]:postfix_list[0]+l]
+        # tmp_post_list = []
+        # tmp_pre_list = []
+        # tmp_tf = 0
         for i in postfix_list:
             if i + l > string_l:
                 continue
@@ -41,22 +50,24 @@ def get_candidate_dict(string, min_l=2, max_l=5, tf=1, ie=0, pmi=1):
             word = string[i:i+l]
             postfix = string[i+l] if i + l < string_l else None
             prefix = string[i-1] if i - 1 >= 0 else None
-            if word != last_word:
-                candidate_dict.update({last_word: [tmp_tf, tmp_pre_list, tmp_post_list]})
-                print last_word, tmp_tf
-                last_word = word
+            if word in candidate_dict:
+                candidate_dict[word][0] += 1
+                candidate_dict[word][1].append(prefix)
+                candidate_dict[word][2].append(postfix)
             else:
-                tmp_post_list.append(postfix)
-                tmp_pre_list.append(prefix)
-                tmp_tf += 1
+                candidate_dict[word] = [1, [], []]
     total_freq = float(total_freq)
-    print "1 done"
+    end = datetime.datetime.now()
+    print ("create candidate_dict done, costs time: %s" % (str(end - start))).upper()
+    start = datetime.datetime.now()
+    print "start computing information entropy and mutual information ..."
     for i in candidate_dict:
         candidate_dict[i][1] = min(get_information_entropy(candidate_dict[i][1]), get_information_entropy(candidate_dict[i][2]))
         candidate_dict[i][2] = get_mutual_information(i, candidate_dict[i][0] / total_freq)
-    print "2 done"
-    candidate_list = sorted(candidate_dict.items(), lambda x, y: cmp(x[1][0] * (x[1][1] + x[1][2]), y[1][0] * (y[1][1] + y[1][2])), reverse=True)
-    candidate_list = [i for i in candidate_list if i[1][0] > tf and i[1][1] > ie and i[1][2] > pmi]
+    end = datetime.datetime.now()
+    print ("computing done, costs time: %s" % (str(end - start))).upper()
+    candidate_list = [(i, candidate_dict[i]) for i in candidate_dict if candidate_dict[i][0] > tf and candidate_dict[i][1] > ie and candidate_dict[i][2] > pmi]    
+    candidate_list = sorted(candidate_list, lambda x, y: cmp(x[1][0] * (x[1][1] + x[1][2]), y[1][0] * (y[1][1] + y[1][2])), reverse=True)
     return candidate_list
 
 if __name__ == "__main__":
